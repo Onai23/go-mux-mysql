f147f9d006e62b5fbd2adef2eaa1ba77ee4571d8
Onai23 <onai.nadapdap.23@gmail.com>
2022-04-05 09:17:56 +0700

    Menambahkan file go.sum, modifikasi file go.mod dan menambahkan konfigurasi pada file app.go

--- app.go
+++ app.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"github.com/gorilla/mux"
+)
+
+//Deklarasi cetakan App
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+//method initialize bertanggung jawab untuk membangun koneksi ke database
+func (a *App) Initialize(user, password, dbName string) {
+
+}
+
+//method run digunakan untuk menjalankan script golang
+func (a *App) Run(addr string) {
 
 }
